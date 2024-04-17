472c1adc0e59680301085d43ff8170e965be4f80
isimmons <isimmons33@gmail.com>
2024-04-17 14:02:45 -0400

    fix multiple alpine instance

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,5 +4,7 @@ import Alpine from 'alpinejs'
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.Alpine = Alpine
-Alpine.start()
+if( ! window.Alpine ) {
+    window.Alpine = Alpine
+    Alpine.start()
+}
