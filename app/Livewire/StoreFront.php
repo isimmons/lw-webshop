910a41457632727e4b6b23787abe149882d3032b
isimmons <isimmons33@gmail.com>
2024-04-17 14:03:11 -0400

    show me the money!

--- app/Livewire/StoreFront.php
+++ app/Livewire/StoreFront.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Product;
+use Attribute;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
 use Livewire\Attributes\Computed;
