9d9184f3b1030e954e46d022375091969c4b0529
isimmons <isimmons33@gmail.com>
2024-04-17 14:08:52 -0400

    style product wrapper

--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,8 +1,8 @@
 <div class="grid grid-cols-4 gap-4 mt-12">
     @foreach($this->products as $product)
-        <div>
+        <div class="bg-white rounded-lg shadow dark:shadow-slate-400 p-4">
             <img src="{{ $product->featured_image->path }}" alt="Picture of {{ $product->name }}">
-            <h2 class="font-medium text-lg text-slate-900 dark:text-slate-200">
+            <h2 class="font-medium text-lg text-slate-800 ">
                 {{ $product->name }}
             </h2>
             <span class=" text-sm text-slate-600 dark:text-slate-400">{{ $product->price }}</span>
