17d2e62f826410e1d9afd8f5287d3601f4453a04
isimmons <isimmons33@gmail.com>
2024-04-30 20:46:14 -0400

    update all the things, add search and paginate

--- app/Livewire/StoreFront.php
+++ app/Livewire/StoreFront.php
@@ -4,16 +4,25 @@ namespace App\Livewire;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\View\View;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class StoreFront extends Component
 {
+    use WithPagination;
+
+    #[Url]
+    public $searchQuery;
+
     #[Computed]
-    public function products(): Collection|array
+    public function products(): LengthAwarePaginator
     {
-        return Product::with(['images', 'featured_image'])->get();
+        return Product::when($this->searchQuery, fn($query) => $query->where('name', 'like', '%' . $this->searchQuery . '%'))
+            ->with(['images', 'featured_image'])->paginate(5);
     }
 
     public function render(): View
--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,12 +1,23 @@
-<div class="grid grid-cols-4 gap-4 mt-12">
-    @foreach($this->products as $product)
-        <x-panel class="relative">
-            <a href="{{ route('product.show', $product) }}" class="absolute inset-0 size-full"></a>
-            <img src="{{ $product->featured_image->path }}" alt="Picture of {{ $product->name }}">
-            <h2 class="font-medium text-lg text-slate-800 ">
-                {{ $product->name }}
-            </h2>
-            <span class=" text-sm text-slate-600 dark:text-slate-400">{{ $product->price }}</span>
-        </x-panel>
-    @endforeach
+<div class="mt-12">
+    <div class="flex justify-between">
+        <h1 class="text-lg font-medium dark:text-slate-300">Our Products</h1>
+        <div>
+            <x-input wire:model.live.debounce="searchQuery" type="text" placeholder="Search Products" />
+        </div>
+    </div>
+    <div class="grid grid-cols-4 gap-4 mt-12">
+        @foreach($this->products as $product)
+            <x-panel class="relative">
+                <a wire:navigate href="{{ route('product.show', $product) }}" class="absolute inset-0 size-full"></a>
+                <img src="{{ $product->featured_image->path }}" alt="Picture of {{ $product->name }}">
+                <h2 class="font-medium text-lg text-slate-800 ">
+                    {{ $product->name }}
+                </h2>
+                <span class=" text-sm text-slate-600 dark:text-slate-400">{{ $product->price }}</span>
+            </x-panel>
+        @endforeach
+    </div>
+    <div class="mt-6">
+        {{ $this->products()->links() }}
+    </div>
 </div>
